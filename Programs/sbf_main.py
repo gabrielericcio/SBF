f1ee46cb5b4b954db8e68ad86c579abcd5b578d5
Gabriele Riccio <gabrieler.iccio@Gabrieles-MBP.home-life.hub>
2024-03-29 13:37:49 +0100

    Reorder run_part1 class

--- Programs/sbf_main.py
+++ Programs/sbf_main.py
@@ -18,15 +18,15 @@ ipython.run_line_magic('autoreload', '2')
 
 import init
 import time
-import sbf_v12_VCC as sbf
+import sbf_classes_v1 as sbf
 import numpy as np
-import sbf_pr_vcc as p_r
+import sbf_pr as p_r
 from astropy.table import Table
 
 t0=time.time()
 # galaxies = ['ic0745','ngc4753','ngc5813','ngc5831','ngc5839']
 # galaxies = ['vcc1025','vcc0828','vcc1146']
-galaxies = ['vcc0122']
+galaxies = ['vcc1025']
 # galaxies = ['vcc1146']
 # galaxies=['vcc0828']
 # bands=['i']
@@ -60,10 +60,10 @@ for i in range(len(galaxies)) :
         init.default() # default for all galaxies
         init_spec = getattr(init, gxy) #specific initialisation for each galaxy
         init_spec()
-        #fwhm[bands[j]]=sbf.run_part1(rms=rms, remove_bkg=True) #IS BUILDING MODEL NOW
+        fwhm[bands[j]]=sbf.run_part1(rms=rms, remove_bkg=True) #IS BUILDING MODEL NOW
         #FWHM IS UPDATED CORRECTLY ONLY WHEN PART 1 IS RUN
         print('FWHM AAAAAA', fwhm)
-        sbf.run_part2(alpha[bands[j]])
+        #sbf.run_part2(alpha[bands[j]])
         
     table=IrsaDust.get_query_table(galaxies[i],section='ebv')
     
@@ -73,12 +73,7 @@ for i in range(len(galaxies)) :
     print(A)
     #####
 
-    # fwhm=[0.61,0.48] # KEEP ONLY WHEN NOT RUNNING PART 1
-    #VCC1025:
-    #fwhm={'g':0.83,'i': 0.58} # KEEP ONLY WHEN NOT RUNNING PART 1
-   
-    #VCC0828:
-    # fwhm={'g':0.79,'i': 0.57} # KEEP ONLY WHEN NOT RUNNING PART 1
+    
 
     #VCC0122:
     fwhm={'g':0.66,'i': 0.59} # KEEP ONLY WHEN NOT RUNNING PART 1
@@ -99,12 +94,12 @@ for i in range(len(galaxies)) :
     # cilow=0.25 NOT USED
     # cihigh=0.75 NOT USED
 
-    sbf.run_part3(galaxies[i],bands[0],bands[1],A[0],A[1],
-                  magzp[bands[0]],magzp[bands[1]],fwhm[bands[0]],fwhm[bands[1]],
-                  match_scale,csmin,mfaint,mbright,threshold)
+    #sbf.run_part3(galaxies[i],bands[0],bands[1],A[0],A[1],
+                  #magzp[bands[0]],magzp[bands[1]],fwhm[bands[0]],fwhm[bands[1]],
+                  #match_scale,csmin,mfaint,mbright,threshold)
 
-    sbf.run_part4(galaxies[i],bands[0],bands[1],magzp[bands[0]],magzp[bands[1]],
-                    fwhm[bands[0]],fwhm[bands[1]], A[1])
+    #sbf.run_part4(galaxies[i],bands[0],bands[1],magzp[bands[0]],magzp[bands[1]],
+                    #fwhm[bands[0]],fwhm[bands[1]], A[1])
     ## PSF Modelling:
     psf_rad_scale=3 # 5 FOR HSC.#4*plate_scale
     nthfactor=20 # nth neighbor selection radius= nthfactor*rad_asec
@@ -112,19 +107,19 @@ for i in range(len(galaxies)) :
 
     psfsize=64
     oversampling=4
-    sbf.run_part5(galaxies[i],bands[0],bands[1],A[0],A[1],magzp[bands[0]],
-                    magzp[bands[1]],fwhm[bands[0]],fwhm[bands[1]],
-                    mfaint,mbright,threshold,cscut,psf_rad_scale,nthfactor,
-                    psfsize,oversampling,rgc_factor)
+    #sbf.run_part5(galaxies[i],bands[0],bands[1],A[0],A[1],magzp[bands[0]],
+                    #magzp[bands[1]],fwhm[bands[0]],fwhm[bands[1]],
+                    #mfaint,mbright,threshold,cscut,psf_rad_scale,nthfactor,
+                    #psfsize,oversampling,rgc_factor)
     
     # sbf.find_annuli(galaxies[i],bands[0],bands[1])
 
         
     # MASKING ANNULUS: Inner and outer radii--- in pix
-    #VCC1025:
-    #ORIGINAL    
-    #in_rad=np.array([36,54,90,162,306,36,36])#*0.187#0.00187 # degrees 36 pix   0.0084 #
-    #out_rad=np.array([54,90,162,306,594,600,300])#*0.187#0.0156 #0.0159 # cannulus 4 162 to 306 pix # degrees 300 pix : cannulus7 from Mik
+    # VCC1025:
+    # ORIGINAL    
+    # in_rad=np.array([36,54,90,162,306,36,36])#*0.187#0.00187 # degrees 36 pix   0.0084 #
+    # out_rad=np.array([54,90,162,306,594,600,300])#*0.187#0.0156 #0.0159 # cannulus 4 162 to 306 pix # degrees 300 pix : cannulus7 from Mik
     in_rad=np.array([236,254,290,162,306,36,36])#*0.187#0.00187 # degrees 36 pix   0.0084 #
     out_rad=np.array([354,390,362,306,594,600,300])#*0.187#0.0156 #0.0159 # cannulus 4 162 to 306 pix # degrees 300 pix : cannulus7 from Mik
 
@@ -141,11 +136,11 @@ for i in range(len(galaxies)) :
     cutout_size=1536
 
 
-    sbf.run_pr_cat(galaxies[i],bands[0],bands[1],magzp[bands[0]],magzp[bands[1]],
-                    fwhm[bands[0]],fwhm[bands[1]], cutout_size, csmin,mfaint,mbright,threshold)
+    #sbf.run_pr_cat(galaxies[i],bands[0],bands[1],magzp[bands[0]],magzp[bands[1]],
+                    #fwhm[bands[0]],fwhm[bands[1]], cutout_size, csmin,mfaint,mbright,threshold)
 
 
-    p_r_ann= p_r.main(galaxies[i], in_rad, out_rad)
+    #p_r_ann= p_r.main(galaxies[i], in_rad, out_rad)
 
     
     #VCC1025
@@ -168,40 +163,40 @@ for i in range(len(galaxies)) :
     p0_med=np.zeros(in_rad.shape)
     p1_med=np.zeros(in_rad.shape)
     
-    for r in range(len(in_rad)):
+    #for r in range(len(in_rad)):
         # print(in_rad[r],out_rad[r])
         
-        print(f"ANNULUS #{r+1}")
+     #   print(f"ANNULUS #{r+1}")
         # POWER SPECTRA ANALYSIS AND COMPARISON:
         # p0[r],p1[r]=sbf.run_part6(galaxies[i],bands[0],bands[1],magzp[bands[0]],magzp[bands[1]],
         #             fwhm[bands[0]],fwhm[bands[1]],in_rad[r], out_rad[r], A[1], cutout_size,r)
 
 
-        p0,p1=sbf.run_part6_starpsf(galaxies[i],bands[0],bands[1],magzp[bands[0]],magzp[bands[1]],
-                    fwhm[bands[0]],fwhm[bands[1]],in_rad[r], out_rad[r], A[1], cutout_size, psf_stars, r)
-        print(p0)
-        print(p1)
-        p0_med[r]=np.median(p0)
-        p1_med[r]=np.median(p1)
-    print("median P_0 by annulus:") 
-    print(p0_med)
-    print("% differences:")
-    print((p0_med-p0_mik)/p0_mik*100)
+      #  p0,p1=sbf.run_part6_starpsf(galaxies[i],bands[0],bands[1],magzp[bands[0]],magzp[bands[1]],
+                    #fwhm[bands[0]],fwhm[bands[1]],in_rad[r], out_rad[r], A[1], cutout_size, psf_stars, r)
+       # print(p0)
+        #print(p1)
+        #p0_med[r]=np.median(p0)
+        #p1_med[r]=np.median(p1)
+    #print("median P_0 by annulus:") 
+    #print(p0_med)
+    #print("% differences:")
+    #print((p0_med-p0_mik)/p0_mik*100)
 
-    print("P_1 by annulus:") 
-    print(p1_med)
+    #print("P_1 by annulus:") 
+    #print(p1_med)
     # print("% differences:")
     # print((p1_med-p1_mik)/p1_mik*100)
-    print("P_r by annulus:") 
-    print(p_r_ann)
-    print("median P_fluc by annulus:") 
-    print(p0_med-p_r_ann)
+    #print("P_r by annulus:") 
+    #print(p_r_ann)
+    #print("median P_fluc by annulus:") 
+    #print(p0_med-p_r_ann)
     
-    t=Table([p0_med,p1_med, p_r_ann,p0_med-p_r_ann], names=('P0','P1','Pr','Pfluc'))
-    print(t)
+    #t=Table([p0_med,p1_med, p_r_ann,p0_med-p_r_ann], names=('P0','P1','Pr','Pfluc'))
+    #print(t)
 
-t1=time.time()
-t=(t1-t0)/60.0
-print ("Total runtime = %f minutes" %t)
+#t1=time.time()
+#t=(t1-t0)/60.0
+#print ("Total runtime = %f minutes" %t)
 
 # %%
